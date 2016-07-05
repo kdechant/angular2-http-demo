acc4910e80d433994fee9a9f5c1999d92c4705ba
Keith Dechant <keith.dechant@gmail.com>
2016-07-05 01:01:32 -0700

    Added create, update, and delete methods for the food objects.
    Note: The API calls here will fail with a 404 error because this repo does not contain any code for a back-end API.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {DemoService} from './demo.service';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'demo-app',
@@ -7,8 +8,9 @@ import {DemoService} from './demo.service';
   <h1>Angular2 HTTP Demo App</h1>
   <h2>Foods</h2>
   <ul>
-    <li *ngFor="let food of foods">{{food.name}}</li>
+    <li *ngFor="let food of foods"><input type="text" name="food-name" [(ngModel)]="food.name"><button (click)="updateFood(food)">Save</button> <button (click)="deleteFood(food)">Delete</button></li>
   </ul>
+  <p>Create a new food: <input type="text" name="food_name" [(ngModel)]="food_name"><button (click)="createFood(food_name)">Save</button></p>
   <h2>Books and Movies</h2>
   <h3>Books</h3>
   <ul>
@@ -25,6 +27,8 @@ export class AppComponent {
   public foods;
   public books;
   public movies;
+
+  public food_name;
 
   constructor(private _demoService: DemoService) { }
 
@@ -53,5 +57,50 @@ export class AppComponent {
       // No error or completion callbacks here. They are optional, but
       // you will get console errors if the Observable is in an error state.
     );
+  }
+
+  createFood(name) {
+    let food = {name: name};
+    this._demoService.createFood(food).subscribe(
+       data => {
+         // refresh the list
+         this.getFoods();
+         return true;
+       },
+       error => {
+         console.error("Error saving food!");
+         return Observable.throw(error);
+       }
+    );
+  }
+
+  updateFood(food) {
+    this._demoService.updateFood(food).subscribe(
+       data => {
+         // refresh the list
+         this.getFoods();
+         return true;
+       },
+       error => {
+         console.error("Error saving food!");
+         return Observable.throw(error);
+       }
+    );
+  }
+
+  deleteFood(food) {
+    if (confirm("Are you sure you want to delete " + food.name + "?")) {
+      this._demoService.deleteFood(food).subscribe(
+         data => {
+           // refresh the list
+           this.getFoods();
+           return true;
+         },
+         error => {
+           console.error("Error deleting food!");
+           return Observable.throw(error);
+         }
+      );
+    }
   }
 }
--- app/demo.service.ts
+++ app/demo.service.ts
@@ -1,24 +1,46 @@
 import {Injectable} from '@angular/core';
-import {Http, Response} from '@angular/http';
+import {HTTP_PROVIDERS, Http, Response, Headers, RequestOptions} from "@angular/http";
 import {Observable} from 'rxjs/Rx';
 
 @Injectable()
 export class DemoService {
 
-  constructor(private http:Http) { }
+    constructor(private http:Http) {
+    }
 
-  // Uses http.get() to load a single JSON file
-  getFoods() {
-    return this.http.get('/app/food.json').map((res:Response) => res.json());
-  }
+    // Uses http.get() to load a single JSON file
+    getFoods() {
+        return this.http.get('/app/food.json').map((res:Response) => res.json());
+    }
 
-  // Uses Observable.forkJoin() to run multiple concurrent http.get() requests.
-  // The entire operation will result in an error state if any single request fails.
-  getBooksAndMovies() {
-    return Observable.forkJoin(
-      this.http.get('/app/books.json').map((res:Response) => res.json()),
-      this.http.get('/app/movies.json').map((res:Response) => res.json())
-    );
-  }
+    // Uses Observable.forkJoin() to run multiple concurrent http.get() requests.
+    // The entire operation will result in an error state if any single request fails.
+    getBooksAndMovies() {
+        return Observable.forkJoin(
+        this.http.get('/app/books.json').map((res:Response) => res.json()),
+        this.http.get('/app/movies.json').map((res:Response) => res.json())
+        );
+    }
+
+    createFood(food) {
+        let headers = new Headers({'Content-Type': 'application/json'});
+        let options = new RequestOptions({headers: headers});
+        let body = JSON.stringify(food);
+        // Note: This is only an example. The following API call will fail because there is no actual API to talk to.
+        return this.http.post('/api/food/', body, headers).map((res:Response) => res.json());
+    }
+
+    updateFood(food) {
+        let headers = new Headers({'Content-Type': 'application/json'});
+        let options = new RequestOptions({headers: headers});
+        let body = JSON.stringify(food);
+        // Note: This is only an example. The following API call will fail because there is no actual API to talk to.
+        return this.http.put('/api/food/' + food.id, body, headers).map((res:Response) => res.json());
+    }
+
+    deleteFood(food) {
+        // Note: This is only an example. The following API call will fail because there is no actual API to talk to.
+        return this.http.delete('/api/food/' + food.id);
+    }
 
 }
